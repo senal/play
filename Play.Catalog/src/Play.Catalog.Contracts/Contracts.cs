e96a03efca5e95be9955b51999bedc6bc60105a4
senal <ranga.fernando@gmail.com>
2022-04-03 18:45:03 +1000

    Update Catalog projects

---
+++ Play.Catalog/src/Play.Catalog.Contracts/Contracts.cs
@@ -0,0 +1,6 @@
+using System;
+namespace Play.Catalog.Contracts {
+    public class CatalogItemCreated {
+
+    }
+}
--- Play.Catalog/src/Play.Catalog.Service/Controllers/ItemsController.cs
+++ Play.Catalog/src/Play.Catalog.Service/Controllers/ItemsController.cs
@@ -1,99 +1,79 @@
-using System.Collections;
 using System;
 using System.Collections.Generic;
-using Microsoft.AspNetCore.Mvc;
-using Play.Catalog.Service.Dtos;
 using System.Linq;
 using System.Threading.Tasks;
+using MassTransit;
+using Microsoft.AspNetCore.Mvc;
+using Play.Catalog.Service.Dtos;
 using Play.Catalog.Service.Entities;
 using Play.Common;
 
-namespace Play.Catalog.Service.Controllers 
-{
+namespace Play.Catalog.Service.Controllers {
     [ApiController]
-    [Route("items")]
-    public class ItemsController : ControllerBase
-    {
-        private readonly IRepository<Item> _itemsRepository; 
-        private static int requestCounter = 0;
-        public ItemsController(IRepository<Item> itemRepository)
-        {
+    [Route ("items")]
+    public class ItemsController : ControllerBase {
+        private readonly IRepository<Item> _itemsRepository;
+        private readonly IPublishEndpoint _publishEndpoint;
+
+        public ItemsController (IRepository<Item> itemRepository, IPublishEndpoint publishEndpoint) {
             _itemsRepository = itemRepository;
+            _publishEndpoint = publishEndpoint;
         }
 
         [HttpGet]
-        public async Task<ActionResult<IEnumerable<ItemDto>>> GetAsync()
-        {
-            requestCounter++;
-            Console.WriteLine($"Request {requestCounter}: Starting...");    
-            if (requestCounter < 2){
-                Console.WriteLine($"Request {requestCounter}: Deplaying...");
-                await Task.Delay(TimeSpan.FromSeconds(10));
-            }
-
-            if (requestCounter <= 4) {
-                Console.WriteLine($"Request {requestCounter}: 500 (Internal Server Error).");
-                return StatusCode(500);
-            }
-            var items = (await _itemsRepository.GetAllAsync())
-                        .Select(x => x.AsDto());
-            
-            Console.WriteLine($"Request {requestCounter}: 200 (OK)).");
-            return Ok(items);
+        public async Task<ActionResult<IEnumerable<ItemDto>>> GetAsync ( ) {
+            var items = (await _itemsRepository.GetAllAsync ( ))
+                .Select (x => x.AsDto ( ));
+            return Ok (items);
         }
 
-        [HttpGet("{id}")]
-        public async Task<ActionResult<ItemDto>> GetByIdAsync(Guid id) 
-        {
-            var item = (await _itemsRepository.GetAsync(id)).AsDto();
-            if(item == null) {
-                return NotFound();
+        [HttpGet ("{id}")]
+        public async Task<ActionResult<ItemDto>> GetByIdAsync (Guid id) {
+            var item = (await _itemsRepository.GetAsync (id)).AsDto ( );
+            if (item == null) {
+                return NotFound ( );
             }
             return item;
         }
 
         [HttpPost]
-        public async Task<ActionResult<ItemDto>> PostAsync(CreateItemDto createItemDto)
-        {
+        public async Task<ActionResult<ItemDto>> PostAsync (CreateItemDto createItemDto) {
             var item = new Item {
                 Name = createItemDto.Name,
                 Description = createItemDto.Description,
                 Price = createItemDto.Price,
                 CreatedDate = DateTimeOffset.UtcNow
             };
-            await _itemsRepository.CreateAsync(item);
-            return CreatedAtAction(nameof(GetByIdAsync), new {id = item.Id}, item);
+            await _itemsRepository.CreateAsync (item);
+            return CreatedAtAction (nameof (GetByIdAsync), new { id = item.Id }, item);
         }
-        
+
         // PUT /items/{id}
-        [HttpPut("{id}")]
-        public async Task<IActionResult> PutAsync(Guid id, UpdateItemDto updateItemDto)
-        {
-            var item = await _itemsRepository.GetAsync(id);
-            if(item == null) {
-                return NotFound();
+        [HttpPut ("{id}")]
+        public async Task<IActionResult> PutAsync (Guid id, UpdateItemDto updateItemDto) {
+            var item = await _itemsRepository.GetAsync (id);
+            if (item == null) {
+                return NotFound ( );
             }
             item.Name = updateItemDto.Name;
             item.Description = updateItemDto.Description;
             item.Price = updateItemDto.Price;
 
-            await _itemsRepository.UpdateAsync(item);
+            await _itemsRepository.UpdateAsync (item);
 
-            return NoContent();
+            return NoContent ( );
         }
 
         // DELETE /items/{id}
-        [HttpDelete("{id}")]
-        public async Task<IActionResult> Delete(Guid id)
-        {
-            var item = await _itemsRepository.GetAsync(id);
-            if(item == null) 
-            {
-                return NotFound();
+        [HttpDelete ("{id}")]
+        public async Task<IActionResult> Delete (Guid id) {
+            var item = await _itemsRepository.GetAsync (id);
+            if (item == null) {
+                return NotFound ( );
             }
 
-            await _itemsRepository.DeleteAsync(id);
-            return NoContent();
+            await _itemsRepository.DeleteAsync (id);
+            return NoContent ( );
         }
     }
 
