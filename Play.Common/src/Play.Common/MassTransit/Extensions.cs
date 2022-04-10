9ef7d6adcafb6d2609f62947e1feb690d732e00a
senal <ranga.fernando@gmail.com>
2022-04-10 14:05:50 +1000

    Removing the inter-service synchronous communication and add retry

--- Play.Common/src/Play.Common/MassTransit/Extensions.cs
+++ Play.Common/src/Play.Common/MassTransit/Extensions.cs
@@ -1,4 +1,6 @@
+using System;
 using System.Reflection;
+using GreenPipes;
 using MassTransit;
 using MassTransit.Definition;
 using Microsoft.Extensions.Configuration;
@@ -20,6 +22,9 @@ namespace Play.Common.MassTransit {
                     var rabbitMQSettings = configuration.GetSection (nameof (RabbitMQSettings)).Get<RabbitMQSettings> ( );
                     configurator.Host (rabbitMQSettings.Host);
                     configurator.ConfigureEndpoints (context, new KebabCaseEndpointNameFormatter (serviceSettings.ServiceName, false));
+                    configurator.UseMessageRetry (retryConfig => {
+                        retryConfig.Interval (3, TimeSpan.FromSeconds (5));
+                    });
                 });
             });
 
--- Play.Inventory/src/Play.Inventory.Service/Controllers/ItemsController.cs
+++ Play.Inventory/src/Play.Inventory.Service/Controllers/ItemsController.cs
@@ -8,62 +8,53 @@ using Play.Inventory.Service.Clients;
 using Play.Inventory.Service.Dtos;
 using Play.Inventory.Service.Entities;
 
-namespace Play.Inventory.Service.Controllers
-{
+namespace Play.Inventory.Service.Controllers {
     [ApiController]
-    [Route("items")]
-    public class ItemsController : ControllerBase
-    {
+    [Route ("items")]
+    public class ItemsController : ControllerBase {
         private readonly IRepository<InventoryItem> _itemRepository;
-        private readonly CatalogClient _catalogClient;
+        private readonly IRepository<CatalogItem> _catalogRepository;
 
-        public ItemsController(IRepository<InventoryItem> itemRepository, CatalogClient catalogClient)
-        {
+        public ItemsController (IRepository<InventoryItem> itemRepository, IRepository<CatalogItem> catalogRepository) {
             _itemRepository = itemRepository;
-            _catalogClient = catalogClient;
+            _catalogRepository = catalogRepository;
         }
 
         [HttpGet]
-        public async Task<ActionResult<IEnumerable<InventoryItemDto>>> GetAsync(Guid userId)
-        {
-            if (userId == Guid.Empty)
-            {
-                return BadRequest();
+        public async Task<ActionResult<IEnumerable<InventoryItemDto>>> GetAsync (Guid userId) {
+            if (userId == Guid.Empty) {
+                return BadRequest ( );
             }
-            var catalogItems = await _catalogClient.GetCatalogItemsAsync();
-            var inventoryItemEntities = await _itemRepository.GetAllAsync(x => x.UserId == userId); 
+            var inventoryItemEntities = await _itemRepository.GetAllAsync (x => x.UserId == userId);
+            var catalogItemIds = inventoryItemEntities.Select (x => x.CatalogItemId);
+            var catalogItemEntities = await _catalogRepository.GetAllAsync (x => catalogItemIds.Contains (x.Id));
 
-            var inventoryItemDtos = inventoryItemEntities.Select(x => 
-            {
-                var catalogItem = catalogItems.Single(catalogItem => catalogItem.Id == x.CatalogItemId);
-                return x.AsDto(catalogItem.Name, catalogItem.Description);
+            var inventoryItemDtos = inventoryItemEntities.Select (x => {
+                var catalogItem = catalogItemEntities.Single (catalogItem => catalogItem.Id == x.CatalogItemId);
+                return x.AsDto (catalogItem.Name, catalogItem.Description);
             });
 
-            return Ok(inventoryItemDtos);
+            return Ok (inventoryItemDtos);
         }
 
         [HttpPost]
-        public async Task<ActionResult> PostAsync(GrantItemsDto grantItemDto)
-        {
-            var inventoryItem = await _itemRepository.GetAsync(x => x.UserId == grantItemDto.UserId && x.CatalogItemId == grantItemDto.CatalogItemId);
-            if ( inventoryItem == null)
-            {
-                inventoryItem = new InventoryItem() 
-                {
-                    CatalogItemId = grantItemDto.CatalogItemId,
-                    UserId = grantItemDto.UserId,
-                    Quantity = grantItemDto.Quantity,
-                    AcquiredDate = DateTimeOffset.UtcNow
+        public async Task<ActionResult> PostAsync (GrantItemsDto grantItemDto) {
+            var inventoryItem = await _itemRepository.GetAsync (x => x.UserId == grantItemDto.UserId && x.CatalogItemId == grantItemDto.CatalogItemId);
+            if (inventoryItem == null) {
+                inventoryItem = new InventoryItem ( ) {
+                CatalogItemId = grantItemDto.CatalogItemId,
+                UserId = grantItemDto.UserId,
+                Quantity = grantItemDto.Quantity,
+                AcquiredDate = DateTimeOffset.UtcNow
                 };
 
-                await _itemRepository.CreateAsync(inventoryItem);
-            } 
-            else {
+                await _itemRepository.CreateAsync (inventoryItem);
+            } else {
                 inventoryItem.Quantity += grantItemDto.Quantity;
-                await _itemRepository.UpdateAsync(inventoryItem);
+                await _itemRepository.UpdateAsync (inventoryItem);
             }
 
-            return Ok();
+            return Ok ( );
         }
 
     }
